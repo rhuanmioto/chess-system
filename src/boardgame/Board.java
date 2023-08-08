81ed791579de502f0ed5cd49bf31035c005a25d9
Rhuan Mioto <miotorhuan@gmail.com>
2023-08-08 17:09:38 -0300

    little improvement in board printing

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,11 +1,11 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
+
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
 			throw new BoardException("Error Creating the board: there must be at least 1 row and 1 column");
@@ -27,14 +27,16 @@ public class Board {
 		if (!positionExists(row, column)) {
 			throw new BoardException("Position not in the board");
 		}
-	return pieces[row][column];
-}
+		return pieces[row][column];
+	}
+
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not in the board");
-		}		
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
+
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position " + position);
@@ -46,15 +48,15 @@ public class Board {
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
-	
+
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
-	
+
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not in the board");
-		}	
+		}
 		return piece(position) != null;
 	}
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -6,7 +6,7 @@ public class ChessPosition {
 
 	private char column;
 	private int row;
-	
+
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8)
 			throw new ChessException("Error instantiating ChessPosition. value options are from a1 to h8.");
@@ -23,13 +23,15 @@ public class ChessPosition {
 	}
 
 	protected Position toPosition() {
-		return new Position(8 - row, column - 'a' );
+		return new Position(8 - row, column - 'a');
 	}
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()),8 - position.getRow());
+		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
 	}
+
 	@Override
 	public String toString() {
-		return ""+ column + row;
+		return "" + column + row;
 	}
 }
